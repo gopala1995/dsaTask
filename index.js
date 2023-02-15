d10930bb35ed331b6b651accc28240c75319a395
Gopala <guptaraja727@gmail.com>
2023-02-15 16:18:35 +0530

    commit

--- index.js
+++ index.js
@@ -23,42 +23,82 @@ let B = "GopalaOf";
 
 // console.log(CommonSubString(A, B));
 
+const obj1 = [
+  {
+    id: 1,
+    name: "ishan",
+    age: 23,
+    fullTimeEmployee: true,
+    designation: "SD2",
+  },
+  {
+    id: 2,
+    name: "akshay",
+    age: 21,
+    fullTimeEmployee: true,
+    designation: "SD2",
+  },
+  {
+    id: 3,
+    name: "parth",
+    age: 20,
+    fullTimeEmployee: false,
+    designation: "Intern",
+  },
+  {
+    id: 4,
+    name: "karan",
+    age: 30,
+    fullTimeEmployee: true,
+    designation: "Tester",
+  },
+];
 
+// const findEmployee = obj.filter((el) => {
+//   if (el.fullTimeEmployee == true && el.designation === "SD2") {
+//     return el;
+//   }
+// });
+// console.log(findEmployee);
 
+let arr = [1, 2, 3, 4, 4,]; 
 
-
-
-const obj = [
-    {
-        id:1,
-        name:"ishan",
-        age:23,
-        fullTimeEmployee: true,
-        designation:"SD2",
-    }, {
-        id:2,
-        name:"akshay",
-        age: 21,
-        fullTimeEmployee: true,
-        designation:"SD2"
-    }, {
-        id:3,
-        name:"parth",
-        age:20,
-        fullTimeEmployee: false,
-        designation:"Intern"
-    }, {
-        id:4,
-        name:"karan",
-        age: 30,
-        fullTimeEmployee: true,
-        designation:"Tester",
-    }
-]
-
-const findEmployee  = obj.filter((el) => {
-   if( el.fullTimeEmployee == true &&  el.designation ==="SD2"){
-      return el
-   }
+  
+let sum = arr.reduce((el,acc)=>{
+    return (el+acc)
 })
-console.log(findEmployee);
+// console.log("sum",sum);
+
+let n = arr.length;
+
+const reapeateArr = (arr, n) => {
+  for (var i = 1; i <= n; i++) {
+    if (arr[i] == i) {
+      return arr[i];
+    }
+  }
+};
+// console.log("Repeat",reapeateArr(arr, n));
+
+const MissingEL = (arr, N) => {
+  let n = arr;
+   
+      let total = Math.floor((N + 1) * (N + 2) / 2);
+      for (let i = 0; i < n.length; i++){
+           total -= n[i];
+      }
+         
+      return(total);
+};
+console.log("Missing",MissingEL(arr, n));
+
+let nums = [0,0,1,1,1,2,2,3,3,4]
+
+
+
+ const Duplicates = nums.filter((el,index)=> {
+   return nums.indexOf(el)==index
+  //
+})
+  //  console.log(Duplicates);
+
